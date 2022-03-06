2b7b726c7e3a24ceda4a1caa87dc8b1c45b8059e
a-angeliev <nachko01@gmail.com>
2022-03-06 17:12:43 +0200

    Uploading Bus schedule problem

--- JS-Application/Exe.Asynchronous/02.Bus-Schedule/app.js
+++ JS-Application/Exe.Asynchronous/02.Bus-Schedule/app.js
@@ -1,17 +1,48 @@
 function solve() {
+  let arriveButton = document.getElementById("arrive");
+  let departButton = document.getElementById("depart");
+  let url = "http://localhost:3030";
+  let infoElement = document.querySelector(".info");
 
-    function depart() {
-        console.log('Depart TODO...');
+  function depart() {
+    if (infoElement.textContent == "Not Connected") {
+      fetch(`${url}/jsonstore/bus/schedule/depot`)
+        .then((res) => res.json())
+        .then((info) => {
+          localStorage.setItem("name", info.name);
+          localStorage.setItem("next", info.next);
+          infoElement.textContent = `Next stop ${info.name}`;
+        });
+    } else {
+      fetch(`${url}/jsonstore/bus/schedule/${localStorage.getItem("next")}`)
+        .then((res) => res.json())
+        .then((info) => {
+          localStorage.setItem("name", info.name);
+          localStorage.setItem("next", info.next);
+
+          infoElement.textContent = `Next stop ${info.name}`;
+        })
+        .catch(err => {
+          infoElement.textContent = "Error";
+          arriveButton.disabled = true;
+          departButton.disabled = true;
+        });
     }
+    
+    arriveButton.disabled = false;
+    departButton.disabled = true;
+  }
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+  function arrive() {
+    infoElement.textContent = `Arriving at ${localStorage.getItem("name")}`;
+    arriveButton.disabled = true;
+    departButton.disabled = false;
+  }
 
-    return {
-        depart,
-        arrive
-    };
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
